8b2e448e608055a787005452e185869ee0f33e56
hcliu <scpaliuhc@163.com>
2021-10-10 09:43:57 +0800

    final version of ssim,ms_ssim,psnr,vif_p,vsi,fsim,gmsd,ms_gmsd,haarpsi,mdsi,mad

--- calculate_MAD.py
+++ calculate_MAD.py
@@ -5,20 +5,19 @@ import sys
 import numpy as np
 def calculate_MAD_append():
     csv_file='scores/'+sys.argv[1]+'_piq.csv'
+    print(csv_file)
     eng = matlab.engine.start_matlab()
     MAD=[]
     df=pd.read_csv(csv_file)
     count=len(df['file'])
-    for id,file in enumerate(df['file']):
-        # if id>3:
-        #     break
+    for id,file in enumerate(df['file']): 
         ref_name=file[0:11]+'.jpg'
         ae_name=file
         r=eng.MAD_index(os.path.join(sys.argv[2],ref_name),
                         os.path.join(sys.argv[3],sys.argv[1],ae_name))
         MAD.append(r['MAD'])
         print(sys.argv[1],f'{(id+1)/count*100:.2f}%',file,r['MAD'])
-        # df['mad']=r['MAD']
+
     MAD=np.array(MAD)
     df['mad']=MAD
     df.to_csv(f'scores/{sys.argv[1]}.csv',index=False)
